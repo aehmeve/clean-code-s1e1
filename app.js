146b68347b268cbbb941caa518264b3c0b725e87
aehmeve <ehmorizehere@gmail.com>
2024-01-06 07:43:16 +0900

    refactor(basic-3.4): changed all tag selectors to class selectors

--- app.js
+++ app.js
@@ -32,19 +32,22 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "task-item";
     label.innerText=taskString;
-    label.className='task';
+    label.className='task label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "input-item";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-item task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "remove";
     deleteButton.appendChild(deleteButtonImg);
 
 
